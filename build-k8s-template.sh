4df1e7ea04918cdc761e72b71f5b622445b53567
Viet Pham <viet@webonyx.com>
2023-05-25 16:04:26 +0700

    Not set namesuffix for existing overlays

--- build-k8s-template.sh
+++ build-k8s-template.sh
@@ -32,8 +32,23 @@ done
 
 set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 
-if [ -z "${NAME_SUFFIX}" ]; then
-  echo "Error during running \"$0\": \"--namesuffix\" is required but was not set or set to the empty string. Exiting..."
+KUSTOMIZE_BASED_PATH=${KUSTOMIZE_BASED_PATH:-k8s}
+KUSTOMIZE_PATH=${KUSTOMIZE_BASED_PATH}/${OVERLAY:-template}
+isTemplate=false
+
+if [ ! -d "$KUSTOMIZE_BASED_PATH" ]; then
+  echo "Expected Kustomize based path at \"$KUSTOMIZE_BASED_PATH\" does not exists"
+  exit 1
+fi
+
+if [ -z "$OVERLAY" ] || [ ! -d "$KUSTOMIZE_PATH" ]; then
+  KUSTOMIZE_PATH=${KUSTOMIZE_BASED_PATH}/template
+  isTemplate=true
+  echo "[Warning] Build using \"template\" overlay at \"${KUSTOMIZE_PATH}\""
+fi
+
+if [ "true" == "$isTemplate" ] && [ -z "${NAME_SUFFIX}" ]; then
+  echo "Error running \"$0\": \"--namesuffix\" is required but was not set or set to the empty string. Exiting..."
   exit 1
 fi
 
@@ -41,24 +56,16 @@ safename () {
   echo $(echo $1 | sed -e 's/[^A-Za-z0-9]//g')
 }
 
-KUSTOMIZE_BASED_PATH=${KUSTOMIZE_BASED_PATH:-k8s}
-if [ ! -d "$KUSTOMIZE_BASED_PATH" ]; then
-  echo "Kustomize based path does not exists"
-  exit 1
-fi
-
-KUSTOMIZE_PATH=${KUSTOMIZE_BASED_PATH}/${OVERLAY:-template}
-if [ ! -d "$KUSTOMIZE_PATH" ]; then
-  KUSTOMIZE_PATH=${KUSTOMIZE_BASED_PATH}/template
-  echo "[Warning] Overlay \"${OVERLAY}\" does not exists. Fallback to \"template\" overlay at \"${KUSTOMIZE_PATH}\""
-fi
-
-
 NAME_SUFFIX=$(safename ${NAME_SUFFIX})
 SOURCE_IMAGE=${SOURCE_IMAGE:-"unifiedselfserviceeastus2crsb2qrpxc.azurecr.io/dataset"}
 
+# start editing
 cd $KUSTOMIZE_PATH
-kustomize edit set namesuffix -- "-${NAME_SUFFIX}"
+
+# Only set namesuffix for template overlay
+if [ "true" == $isTemplate ]; then
+  kustomize edit set namesuffix -- "-${NAME_SUFFIX}"
+fi
 
 if [ -n "${IMAGE}" ]; then
   echo "Override image with: \"${IMAGE}\""
@@ -68,4 +75,6 @@ fi
 echo "Building template..."
 kustomize build . > k8s.yaml
 
-echo "##vso[task.setvariable variable=MANIFEST_PATH]${KUSTOMIZE_PATH}/k8s.yaml"
+MANIFEST_PATH="${KUSTOMIZE_PATH}/k8s.yaml"
+echo "##vso[task.setvariable variable=MANIFEST_PATH]${MANIFEST_PATH}"
+echo "Exported: MANIFEST_PATH=${MANIFEST_PATH}"
