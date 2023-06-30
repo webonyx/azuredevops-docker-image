98f2537b6c81f256b0be723d636475dc70902c8c
jlaprise <jared@webonyx.com>
2023-06-29 19:06:17 -0700

    Change package name

--- sync-to-acr.sh
+++ sync-to-acr.sh
@@ -2,5 +2,5 @@
 
 ACR_NAME=${ACR_NAME:-unifiedselfserviceeastus2crsb2qrpxc}
 docker pull webonyx/azuredevops-toolbox:latest --platform amd64
-docker tag webonyx/azuredevops-toolbox:latest $ACR_NAME.azurecr.io/toolbox:latest
-docker push $ACR_NAME.azurecr.io/toolbox:latest
+docker tag webonyx/azuredevops-toolbox:latest $ACR_NAME.azurecr.io/azuredevops-toolbox:latest
+docker push $ACR_NAME.azurecr.io/azuredevops-toolbox:latest
